e7689b3dc179aa5f0a782f64248cf25c7d2ab852
unknown <Chelsee 8@CJH-computer.(none)>
2012-05-20 16:40:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoofEstimates::Application.config.secret_token = '15fd81e7a98a65e53b27718bc7ff7e39c6336b3c6ba2b848753888c514bfb52d8e0fe4df6b100930e6287928a4876038dcfbafff5c17eebf4159ba882c46cd41'
