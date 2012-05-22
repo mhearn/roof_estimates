6ce6a57c34d6f64e16f5eec9869bdf4676600f1e
Michael Hearn <michael.e.hearn@gmail.com>
2012-05-22 11:10:28 -0400

    Added tests for links. Normalized Testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,31 +4,54 @@ describe "Static Pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Contact Us"
+    page.should have_selector 'title', text: full_title('Contact Us')
+    click_link "Services"
+    page.should have_selector 'title', text: full_title('Services')
+    click_link "Home"
+    click_link "Register Now!"
+    page.should have_selector 'title', text: full_title('Sign Up')
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { 'eXact Roof Estimates' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1', text: 'eXact Roof Estimates') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: 'Home' }
   end
 
   describe "About page" do
   	before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
 
-    it { should have_selector('h1', text: 'About') }
-    it { should have_selector('title', text: full_title('About')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
   	before { visit contact_path }
+    let(:heading) { 'Contact Us' }
+    let(:page_title) { 'Contact Us' }
 
-    it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_selector('title', text: full_title('Contact Us')) } 
+    it_should_behave_like "all static pages"
   end
 
   describe "Services page" do
   	before { visit services_path }
+    let(:heading) { 'Services' }
+    let(:page_title) { 'Services' }
 
-    it { should have_selector('h1', text: 'Services') }
-    it { should have_selector('title', text: full_title('Services')) }
+    it_should_behave_like "all static pages"
   end
 end
